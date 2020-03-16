dccf3053495de54b65581d9a1a3235050047d6fb
abonello <ab@anthonybonello.co.uk>
2020-03-16 08:58:25 +0000

    Updates

---
+++ precache-manifest.908d508b4e4cbec71060d75ca5e9e52c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea0fe121ae8253593b0421625bcd8a1",
+    "url": "/simplyspectacular/index.html"
+  },
+  {
+    "revision": "53609883cbdf6fe2be27",
+    "url": "/simplyspectacular/static/css/main.487d0a9a.chunk.css"
+  },
+  {
+    "revision": "453e3fbb8c3711ce3a81",
+    "url": "/simplyspectacular/static/js/2.04c92df4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/simplyspectacular/static/js/2.04c92df4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53609883cbdf6fe2be27",
+    "url": "/simplyspectacular/static/js/main.8798d2de.chunk.js"
+  },
+  {
+    "revision": "b81e5a794a4046d1a3c9",
+    "url": "/simplyspectacular/static/js/runtime-main.344475ad.js"
+  },
+  {
+    "revision": "7e44d96b751b12366cfe317bf8de7ef4",
+    "url": "/simplyspectacular/static/media/LogoBurgandy.7e44d96b.png"
+  },
+  {
+    "revision": "f7465648066f26acdc6e5d8af2d20846",
+    "url": "/simplyspectacular/static/media/LogoBurgandyTransp.f7465648.png"
+  },
+  {
+    "revision": "e9a377c4cff52c743d9cce38b7ba1797",
+    "url": "/simplyspectacular/static/media/LogoCocoTransp.e9a377c4.png"
+  },
+  {
+    "revision": "2b6186041b1f379e923740a18cd7b674",
+    "url": "/simplyspectacular/static/media/LogoCountryGreyTransp.2b618604.png"
+  },
+  {
+    "revision": "6d44a58c4d8235b0026f0ca7bfebfdae",
+    "url": "/simplyspectacular/static/media/LogoGraphiteTransp.6d44a58c.png"
+  },
+  {
+    "revision": "ad69da42d14709220534d23a519771ce",
+    "url": "/simplyspectacular/static/media/LogoPalomaGreyTransp.ad69da42.png"
+  },
+  {
+    "revision": "0ba2257c71e8731215d7c21d051ef34b",
+    "url": "/simplyspectacular/static/media/LogoRodwellTransp.0ba2257c.png"
+  },
+  {
+    "revision": "ccb780dfe44dbc9856d33927e0d81551",
+    "url": "/simplyspectacular/static/media/LogoTiltonTransp.ccb780df.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simplyspectacular/static/media/logo.5d5d9eef.svg"
+  }
+]);
